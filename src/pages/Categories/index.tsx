5dad41b421e21649c51d4f36523488a937f1cc28
Lunes0 <lunes6660@gmail.com>
2025-08-23 22:17:13 -0300

    Parte 1 feita

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,96 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import residentEvil from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'teste',
+    description: 'teste',
+    image: residentEvil,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Resident Evil'
+  },
+  {
+    id: 2,
+    category: 'teste',
+    description: 'teste',
+    image: diablo,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Diablo'
+  },
+  {
+    id: 3,
+    category: 'teste',
+    description: 'teste',
+    image: zelda,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Zelda'
+  },
+  {
+    id: 7,
+    category: 'teste',
+    description: 'teste',
+    image: starWars,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Star Wars'
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'teste',
+    description: 'teste',
+    image: residentEvil,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Resident Evil'
+  },
+  {
+    id: 6,
+    category: 'teste',
+    description: 'teste',
+    image: diablo,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Diablo'
+  },
+  {
+    id: 7,
+    category: 'teste',
+    description: 'teste',
+    image: zelda,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Zelda'
+  },
+  {
+    id: 8,
+    category: 'teste',
+    description: 'teste',
+    image: starWars,
+    infos: ['-10%', 'R$ 150'],
+    system: 'Windows',
+    title: 'Star Wars'
+  }
+]
+
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
